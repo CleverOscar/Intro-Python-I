3923ce9841e92ef7b8358ba26b0b72a27294343d
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-04-08 21:18:14 -0500

    13 complete

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+f = open('foo.txt', 'r')
+
+print(f.read())
+
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+#b = open('bar.txt', "x").write("Do you bite your thumb at us, sir?, I do bite my thumb, sir. Do you bite your thumb at us, sir?").close()
+b = open('bar.txt', 'r')
+print(b.read())
